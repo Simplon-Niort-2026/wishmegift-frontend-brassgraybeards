95ef7a4ec006fdfd150c45cf5d096c335be3ae45
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-06-23 16:49:08 +0200

    AddGiftButton basis

--- src/components/AddGiftButton.tsx
+++ src/components/AddGiftButton.tsx
@@ -1,6 +1,7 @@
 // Action to implement
 interface AddGiftButtonProps {
     caption: string;
+    // Action need to receive an arrowed function (and may receive parameters)
     action: React.MouseEventHandler<HTMLElement>;
 };
 
