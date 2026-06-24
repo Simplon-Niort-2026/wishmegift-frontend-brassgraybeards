45373070f7a20d310cde470ae69401eb4a45c6ee
Pierre <66121468+Zatigan@users.noreply.github.com>
2026-06-24 16:34:33 +0200

    Feature/11 bandeau droite (#35)
    * feat: coloredBanner created
    
    * feat: header finished
    
    * chore: deleted dead code
    
    * feat: update font and logo handling
    
    ---------
    
    Co-authored-by: BOULET Pierre <pierre.boulet@maif.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,33 @@
 import "./App.css";
+import "./assets/fonts/fonts.css"
+import Header from './components/Header/Header.tsx';
 import Checkbox from "./components/Checkbox";
-import logo from "./assets/WMG.jpg"
 import Breadcrumb from "./components/Breadcrumb.tsx";
-import LeftBanner from "./components/BannerLeft.tsx";
 
 export default function App() {
 
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <Breadcrumb
+          items={[
+            { label: 'Home', href: '/' },
+            { label: 'Ma Liste', href: '/list' },
+            {
+              label: 'cadeau',
+              href: ""
+            },
 
-    return (
-        <>
-            <header>
-                <LeftBanner>
-                    <div className="logo-container">
-                        <img
-                            src={logo}
-                            alt="logo"
-                        />
-                    </div>
-                </LeftBanner>
-                <div className="logo-container">
-                    <img
-                        src={logo}
-                        alt="logo"
-                    />
-                </div>
-            </header>
-            <main>
-                <Breadcrumb
-                    items={[
-                        {label: 'Home', href: '/'},
-                        {label: 'Ma Liste', href: '/list'},
-                        {
-                            label: 'cadeau',
-                            href: ""
-                        },
-
-                    ]}
-                />
-                <div className="container">
-                    <div className="row">
-                        <Checkbox/>
-                        <span>Réserver</span>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
-
-
+          ]}
+        />
+        <div className="container">
+          <div className="row">
+            <Checkbox />
+            <span>Réserver</span>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css'
+import BannerLeft from "./BannerLeft";
+
+export default function Header() {
+ return (
+  <section className="full-banner">
+   <BannerLeft></BannerLeft>
+  </section>
+ )
+}
